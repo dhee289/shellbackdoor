5dedd40bef42ee310b2eb853cb4909ed127a45db
/Outsiders <78004796+Outsiders289@users.noreply.github.com>
2024-03-26 14:13:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hacked by ./Outsiders
+<html>
+<head>
+<title>Hacked By ./Outsiders</title>
+</head>
+<br>
+<br>
+<center><img src="http://zonehmirrors.org/defaced/2021/01/06/gudgeon.org/64.media.tumblr.com/31775f37478472404971b448e5d01f52/tumblr_mji477MPiV1rb55x4o1_500.gif">
+<br>
+<br><b>
+<center><font size="5"><br>Hacked By ./Outsiders | DepokBlackhat</font><br><br>stilln00b
+</html>
