b6d7adc73574756dba033f4ab519d891f93fdf4b
/Outsiders <78004796+dhee289@users.noreply.github.com>
2024-06-21 14:06:15 +0700

    Update login.php

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if (is_logged_in()) {
     // Display login form if not logged in
     if (isset($_POST['password'])) {
         $entered_password = $_POST['password'];
-        $hashed_password = '$2y$10$e4iHVsmwucrxJaaIklKsM.F05zSNmt5s0DROMXmuR6Ra2cvtTONqq'; // Replace this with your MD5 hashed password
+        $hashed_password = '155e81f10ea3eb530b640b3bf25c56a1'; // Replace this with your MD5 hashed password
         if (md5($entered_password) === $hashed_password) {
             // Password is correct, set a cookie to indicate login
             setcookie('user_id', 'user123', time() + 3600, '/'); // Ganti 'user123' dengan nilai yang sesuai
